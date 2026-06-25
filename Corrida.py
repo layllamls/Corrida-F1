f87b0088bd210480395e856466911b9b9bdc078d
Laylla Fontes <layllamlsfontes@gmail.com>
2026-06-25 14:36:27 -0300

    corrida

--- Corrida.py
+++ Corrida.py
@@ -2,14 +2,16 @@ import abc
 import random
 
 class Veiculo(abc.ABC):
-    def __init__(self, marca, ano, modelo, velocidade_max):
+    def __init__(self, marca, modelo, velocidade_max):
         self.__marca = marca
-        self.__ano = ano
         self.modelo = modelo
         self.__velocidade_max = velocidade_max
 
     def get_velocidade_max(self):
         return self.__velocidade_max
+    
+    def set_velocidade_max(self, nova_velocidade):
+        self.__velocidade_max = nova_velocidade
         
     def acelerar(self):
         pass
@@ -24,26 +26,17 @@ class Veiculo(abc.ABC):
 class Piloto:
     def __init__(self, nome, habilidade):
         self.nome = nome
-        self.__habilidade = habilidade
         self.__vitorias = 0
-
-    def get_habilidade(self):
-        return self.__habilidade
     
     def get_vitorias(self):
         return self.__vitorias
     
-    def adicionar_bonus(self):
-        nova_habilidade = self.get_habilidade() + 5
-        self.__habilidade = nova_habilidade
-        return f"{self.nome} agora possui habilidade igual a: {nova_habilidade}"
-
-    def adicionar_vitoria(self):
+    def set_vitorias(self):
         self.__vitorias += 1 
 
 class Carro(Veiculo):
-    def __init__(self, marca, ano, modelo, velociade_max, piloto):
-        super().__init__(marca, ano, modelo, velociade_max)
+    def __init__(self, marca, modelo, velociade_max, piloto):
+        super().__init__(marca, modelo, velociade_max)
         self.piloto = piloto
         self.distancia = 0
 
@@ -52,4 +45,57 @@ class Carro(Veiculo):
         self.distancia += velocidade
         return f"{self.modelo} acelerando a {velocidade} km/h (Distancia: {self.distancia})"
     
+class Mcqueen:
+    def __init__(self, velocidade_max, piloto):
+        self.__velocidade_max = velocidade_max
+        self.piloto = piloto
+        self.distancia = 0
 
+    def get_velocidade_max(self):
+        return self.__velocidade_max
+    
+    def set_velocidade_max(self, nova_velocidade):
+        self.__velocidade_max = nova_velocidade
+    
+    def acelerar(self):
+        velocidade = random.randint(self.get_velocidade_max() - 40, self.get_velocidade_max())
+        self.distancia += velocidade
+        return f"Mcqueen acelerando a {velocidade} km/h (Distancia: {self.distancia})"
+    
+class Equipe:
+    def __init__(self, nome):
+        self.nome = nome
+        self.__participantes = []
+
+    def get_participantes(self):
+        return self.__participantes
+    
+    def adicionar_participantes(self, participante):
+        self.__participantes.append(participante)
+
+    def listar_participantes(self):
+        for participante in self.get_participantes():
+            print(f"Carro: {participante.modelo} | Piloto: {participante.piloto.nome}")
+
+class Corrida:
+    def __init__(self, participantes, pista):
+        self.participantes = participantes
+        self.distancia_total = pista.comprimento
+        self.resultado = []
+
+    def iniciar(self):
+        print("Corrida iniciada!!\n")
+        while len(self.resultado) < len(self.participantes):
+            for participante in self.participantes:
+                if participante not in self.resultado:
+                    print(participante.acelerar())
+                    if participante.distancia >= self.distancia_total:
+                        print(f"{participante.modelo} chegou!")
+                        self.resultado.append(participante)
+
+        print("\nCorrida finalizada!")
+        
+
+class Pista:
+    def __init__(self, nome, comprimento):
+        pass
