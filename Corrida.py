d125ab18e62a098c32dfd06cd89ed4b207a5d1e9
Laylla Fontes <layllamlsfontes@gmail.com>
2026-06-25 23:46:43 -0300

    Novas classes

--- Corrida.py
+++ Corrida.py
@@ -1,30 +1,34 @@
 import abc
 import random
 
+
 class Veiculo(abc.ABC):
-    def __init__(self, marca, modelo, velocidade_max):
+    def __init__(self, marca, modelo,piloto, velocidade_max, combustivel_max, combustivel_atual):
         self.__marca = marca
         self.modelo = modelo
+        self.piloto = piloto
         self.__velocidade_max = velocidade_max
-
+        self.__combustivel_max = combustivel_max
+        self.__combustivel_atual = combustivel_atual
+    
     def get_velocidade_max(self):
         return self.__velocidade_max
     
-    def set_velocidade_max(self, nova_velocidade):
-        self.__velocidade_max = nova_velocidade
-        
+    def get_combustivel(self):
+        return self.__combustivel_atual
+    
+    def set_combustivel(self, valor):
+        self.__combustivel_atual = valor
+    
+    def get_combustivel_max(self):
+        return self.__combustivel_max
+
+    @abc.abstractmethod
     def acelerar(self):
         pass
 
-"""CLASSES JOGO DE CORRIDA
-- Carro
-- Piloto
-- Equipe
-- Pista
-- Corrida
-"""
 class Piloto:
-    def __init__(self, nome, habilidade):
+    def __init__(self, nome):
         self.nome = nome
         self.__vitorias = 0
     
@@ -32,70 +36,186 @@ class Piloto:
         return self.__vitorias
     
     def set_vitorias(self):
-        self.__vitorias += 1 
+        self.__vitorias += 1
+
+class Pneu:
+    def __init__(self):
+        self.desgaste = 0
+    
+    def desgastar(self, quantidade):
+        self.desgaste += quantidade
+        if self.desgaste > 100:
+            self.desgaste = 100
+    
+    def trocar(self):
+        self.desgaste = 0
+
+class Pitstop:
+    def __init__(self, bomba):
+        self.bomba = bomba
+    
+    def realizar(self, carro):
+        tempo = random.randint(5, 15)
+        
+        for pneu in carro.pneus:
+            pneu.trocar()
+
+        carro.tempo_box += tempo
+
+        combustivel_antes = carro.get_combustivel()
+        quantidade = carro.get_combustivel_max() - carro.get_combustivel()
+        self.bomba.abastecer(quantidade,carro)
+        
+        print(
+            f"{carro.modelo} entrou no box ({tempo}s) | "
+            f"Combustível: {combustivel_antes}L --> {carro.get_combustivel()}L")
 
 class Carro(Veiculo):
-    def __init__(self, marca, modelo, velociade_max, piloto):
-        super().__init__(marca, modelo, velociade_max)
-        self.piloto = piloto
+    def __init__(self, marca, modelo, piloto, velocidade_max, combustivel_max, combustivel_atual):
+        super().__init__(marca, modelo,piloto, velocidade_max, combustivel_max, combustivel_atual)
         self.distancia = 0
-
-    def acelerar(self):
-        velocidade = random.randint(self.get_velocidade_max() - 40, self.get_velocidade_max())
-        self.distancia += velocidade
-        return f"{self.modelo} acelerando a {velocidade} km/h (Distancia: {self.distancia})"
+        self.pneus = [Pneu() for i in range(4)]
+        self.tempo_box = 0
     
-class Mcqueen:
-    def __init__(self, velocidade_max, piloto):
-        self.__velocidade_max = velocidade_max
-        self.piloto = piloto
-        self.distancia = 0
+    def acelerar(self):
+        if self.get_combustivel() <= 0:
+            return f"{self.modelo} ficou sem combustível!"
 
+        velocidade = random.randint(self.get_velocidade_max() - 40, self.get_velocidade_max())
+        
+        self.distancia += velocidade
+        
+        consumo = velocidade * 0.02
+       
+        novo_combustivel = self.get_combustivel() - consumo
+        
+        self.set_combustivel(max(0, novo_combustivel))
+        
+        if velocidade >= 250:
+            desgaste = 5
+        
+        elif velocidade >= 200:
+            desgaste = 3
+        
+        else:
+            desgaste = 1
+        
+        for pneu in self.pneus:
+            pneu.desgastar(desgaste)
+
+        return f"{self.modelo} acelerando a {velocidade} km/h (Distância: {self.distancia})"
+    
+    def precisa_pitstop(self):
+        desgaste_pneus = any(pneu.desgaste >= 70 for pneu in self.pneus)
+        pouco_combustivel = self.get_combustivel() <= 15
+        return desgaste_pneus or pouco_combustivel
+
+class CarroEsportivo:
+    def __init__(self, marca, modelo, piloto, velocidade_max, combustivel_max, combustivel_atual):
+        self.__marca = marca
+        self.modelo = modelo
+        self.piloto = piloto
+        self.__velocidade_max = velocidade_max
+        self.__combustivel_max = combustivel_max
+        self.__combustivel_atual = combustivel_atual
+        self.distancia = 0
+        self.pneus = [Pneu() for c in range(4)]
+        self.tempo_box = 0
+    
     def get_velocidade_max(self):
         return self.__velocidade_max
     
-    def set_velocidade_max(self, nova_velocidade):
-        self.__velocidade_max = nova_velocidade
+    def get_combustivel(self):
+        return self.__combustivel_atual
+    
+    def set_combustivel(self, novo_combustivel):
+        self.__combustivel_atual = novo_combustivel
+    
+    def get_combustivel_max(self):
+        return self.__combustivel_max
     
     def acelerar(self):
-        velocidade = random.randint(self.get_velocidade_max() - 40, self.get_velocidade_max())
+        if self.get_combustivel() <= 0:
+            return f"{self.modelo} ficou sem combustível!"
+        velocidade = random.randint(self.get_velocidade_max() - 20, self.get_velocidade_max())
         self.distancia += velocidade
-        return f"Mcqueen acelerando a {velocidade} km/h (Distancia: {self.distancia})"
+        consumo = velocidade * 0.04
+        novo_combustivel = self.get_combustivel() - consumo
+        self.set_combustivel(max(0, novo_combustivel))
+        desgaste = 4
+        if velocidade >= 280:
+            desgaste = 6
+
+        for pneu in self.pneus:
+            pneu.desgastar(desgaste)
+        return f"{self.modelo} acelerando a {velocidade} km/h (Distancia: {self.distancia})"
+
+    def precisa_pitstop(self):
+        desgaste_pneus = any(pneu.desgaste >= 70 for pneu in self.pneus)
+        pouco_combustivel = self.get_combustivel() <= 15
+        return desgaste_pneus or pouco_combustivel
     
 class Equipe:
     def __init__(self, nome):
         self.nome = nome
         self.__participantes = []
-
-    def get_participantes(self):
-        return self.__participantes
-    
+        
     def adicionar_participantes(self, participante):
         self.__participantes.append(participante)
 
     def listar_participantes(self):
-        for participante in self.get_participantes():
-            print(f"Carro: {participante.modelo} | Piloto: {participante.piloto.nome}")
+        for p in self.__participantes:
+            print(f"Carro: {p.modelo} | Piloto: {p.piloto.nome}")
+
+class Pista:
+    def __init__(self, nome, comprimento):
+        self.nome = nome
+        self.__comprimento = comprimento
+    def get_comprimento(self):
+        return self.__comprimento
 
 class Corrida:
     def __init__(self, participantes, pista):
         self.participantes = participantes
-        self.distancia_total = pista.comprimento
-        self.resultado = []
+        self.distancia_total = pista.get_comprimento()
 
     def iniciar(self):
+        self.resultado = []
+
+        for c in self.participantes:
+            c.distancia = 0
         print("Corrida iniciada!!\n")
+
         while len(self.resultado) < len(self.participantes):
-            for participante in self.participantes:
-                if participante not in self.resultado:
-                    print(participante.acelerar())
-                    if participante.distancia >= self.distancia_total:
-                        print(f"{participante.modelo} chegou!")
-                        self.resultado.append(participante)
+            for carro in self.participantes:
+                if carro not in self.resultado:
+                    print(carro.acelerar())
+                    if carro.precisa_pitstop():
+                        pitstop.realizar(carro)
+                    
+                    if carro.distancia >= self.distancia_total:
+                        print(f"{carro.modelo} chegou!")
+                        self.resultado.append(carro)
 
+        self.resultado[0].piloto.set_vitorias()
         print("\nCorrida finalizada!")
-        
+        print("---Classificação---")
+        lugar_podio = 1
+        for c in self.resultado:
+            print(f"{lugar_podio}° - {c.modelo} | Piloto: {c.piloto.nome}")
+            lugar_podio +=1
 
-class Pista:
-    def __init__(self, nome, comprimento):
-        pass
+class Bomba_Combustivel:
+    def __init__(self, combustivel_atual):
+        self.combustivel_atual = combustivel_atual
+
+    def abastecer(self, quantidade, carro):
+        if quantidade > self.combustivel_atual:
+            print("A bomba não possui combustível suficiente!")
+            return
+        
+        carro.set_combustivel(carro.get_combustivel() + quantidade)
+        self.combustivel_atual -= quantidade
+
+bomba = Bomba_Combustivel(3000)
+pitstop = Pitstop(bomba)
